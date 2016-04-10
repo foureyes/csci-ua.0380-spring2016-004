b189bfd5d0292c70a97ef4000bdbce7bd24344e3
Joe Versoza <jversoza@cs.nyu.edu>
2016-04-09 23:44:08 -0400

    add talk_to_me_server.py

--- assignments/hw06/web_app.py
+++ assignments/hw06/web_app.py
@@ -3,59 +3,73 @@ import random
         
 # routes contains mappings of paths to functions
 # for example, the path /dice would map to a function called dice:
-# {'/dice': dice}
+# {'/dice': dice,
+# '/': home}
 # that function will be called to generate a response
 routes = {}
 
-def route(method, path):
+def route(path):
     """decorator (with parameter) for used to map a path to a function
     """
     def decorator(old_f):
         # add path and function to routes dictionary
-        routes[method, path] = old_f
+        routes[path] = old_f
 
         # just give back the old function unmodified
         return old_f
 
     return decorator
 
-@route('GET', '/')
-def home(req, res):
+##########
+# ROUTES #
+##########
+@route('/')
+def home(req):
+    res = Response(200)
     html = 'The homepage! '
     html += 'Check out <a href="/creatures">creatures</a> '
     html += 'or <a href="/dice">dice</a>.'
     res.set_body(html)
     return res
 
-@route('GET', '/creatures')
-def creatures(req, res):
-    creatures = ["griffin", "zombie", "skeleton", "unicorn", "yeti"]
-    html = 'You encounter these creatures! <ul>'
-    for i in range(random.randint(1, 4)):
-        html += '<li>{}</li>'.format(creatures[random.randint(0, len(creatures) - 1)])
-    html += '</ul>'
-    res.set_body(html)
-    return res
+@route('/creatures')
+def creatures(req):
+    """displays a page of random creatures in an unordered list!
+    """
 
-@route('GET', '/dice')
-def dice(req, res):
+    # TODO: WRITE YOUR IMPLEMENTATION HERE!
+    # (remove pass below)
+    pass
+
+@route('/dice')
+def dice(req):
+    res = Response(200)
     html = "Dice Roll: {}".format(random.randint(1, 6))
     res.set_body(html)
     return res
 
 def handle_request(http_request):
-    req = Request(http_request)
-    route_handler = routes.get((req.method, req.path))
+    """takes an http request as a string and gives backs an http response
+    as a string
+    """
 
-    if route_handler and req.headers['Host'] == 'localhost:5000':
-        res = Response(200)
-        res = route_handler(req, res)
-        res.set_header('Content-Type', 'text/html')
-    elif req.headers['Host'] != 'localhost:5000':
-        res = Response(400)
-        res.body = "Bad Request"
-    elif not route_handler:
-        res = Response(404)
-        res.body = "Not Found"
-    return str(res)
+    # TODO: WRITE YOUR IMPLEMENTATION HERE!
 
+    # 1. create a Request object based on the incoming http request
+
+    # 2. based on the path of the http request, get the right "route"
+    #    (function that will generate the appropriate response... that is
+    #    one of the functions above)
+
+    # 3. if the route exists, call the appropriate function with the
+    #    request object as a parameter... add any headers necessary
+    #    to the resulting Response object
+
+    # 4. if the route doesn't exist, create a Response object with a
+    #    status code of 404 and a body of "Not Found"
+
+    # 5. give back the resulting response object from the either of the
+    #    two previous steps as a string (just use str(...))
+
+    # 6. (remove pass below)
+    pass
--- assignments/hw06/web_objects.py
+++ assignments/hw06/web_objects.py
@@ -7,8 +7,8 @@ class Request:
     Host: localhost:5000
     Accepts: text/html
 
-    Then a Request object representing the above request (where req is an
-    instance of Request):
+    Then a Request object representing the above request (where req is the
+    instance name):
 
     req.path --> "/dice"
     req.method --> "/GET"
@@ -18,99 +18,13 @@ class Request:
         "Host": "localhost:5000",
         "Accepts": "text/html"
     }
-
+    req.body --> "" (the request did not have a body)
     """
+    # TODO: WRITE YOUR IMPLEMENTATION HERE!
 
-    def __init__(self, request_text):
-        self.parse_request(request_text)
-        
-    def parse_request(self, request_text):
-        """Extract path, method and http version from string version of HTTP
-        request. Save in instance variables.
-        """
-        # grab the first line (split by carriage return \r and newline \n - 
-        # http specs mention that \r\n represents newlines)
-        request_parts = request_text.split('\r\n')
-        parts = request_parts[0].split(' ')
-        self.method, self.path, self.http_version = parts
-
-        headers = {}
-        line_num = 1
-        while ':' in request_parts[line_num]:
-            h = request_parts[line_num]
-            i = h.index(':')
-            name = h[:i].strip()
-            value = h[i + 1:].strip()
-            headers[name] = value
-            line_num += 1
-        self.headers = headers
-        self.body = request_parts[line_num + 1]
-
-    def __str__(self):
-        s = "{} {} {}\r\n".format(self.method, self.path, self.http_version)
-        s += "\r\n".join(["{}: {}".format(k, v) for k, v in self.headers.items()])
-        s += "\r\n\r\n{}".format(self.body)
-        return s
 
 class Response:
-
-    STATUS_TEXT = {
-        200: "OK", 
-        400: "Bad Request", 
-        404: "Not Found", 
-        500: "Server Error"
-    }
-
-    def __init__(self, status, http_version="HTTP/1.1"):
-        self.status = status if status in Response.STATUS_TEXT else 200
-        self.http_version = http_version
-        self.headers = {}
-        self.body = ''
-
-    def set_header(self, header_name, header_value):
-        self.headers[header_name] = header_value
-
-    def set_body(self, body):
-        self.body = body
-    
-    def __str__(self):
-        s = "{} {} {}\r\n".format(self.http_version, self.status, Response.STATUS_TEXT[self.status])
-        s += "\r\n".join(["{}: {}".format(k, v) for k, v in self.headers.items()])
-        s += "\r\n\r\n{}".format(self.body)
-        return s
-        
-        
-if __name__ == '__main__':
-    
-    """
-    GET /dice HTTP/1.1
-    User-Agent: p4a browser ftw! 
-    Host: localhost:5000
-    Accepts: text/html
-
-    """
-
-    http_req = "GET /dice HTTP/1.1\r\nUser-Agent: p4a browser ftw!\r\nHost: localhost:5000\r\nAccepts: text/html\r\n\r\n"
-    req = Request(http_req)
-    print(req.method)
-    print(req.path)
-    print(req.headers)
-    print(req.body)
-    print(req)
-
-    """
-    POST /stuff/add HTTP/1.1
-    User-Agent: p4a browser ftw!
-    Host: localhost:5000
-    
-    foo=bar
-    """
-    http_req = "POST /stuff/add HTTP/1.1\r\nUser-Agent: p4a browser ftw!\r\nHost: localhost:5000\r\n\r\nfoo=bar"
-    req = Request(http_req)
-    print(req.path)
-    print(req.headers)
-    print(req.body)
-    print(req)
+    """An object that represents an HTTP response. Example usage:
 
     res = Response(200)
     res.set_header('Content-Type', 'text/html')
@@ -118,9 +32,21 @@ if __name__ == '__main__':
     res.set_body('<h1>stuff</h1>')
     print(res)
 
-    res = Response(777)
-    res.set_body('everything is fine!')
-    print(res)
+    # prints out...
 
-    print(Response.STATUS_TEXT[200])
-    print(Response.STATUS_TEXT[404])
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Server: p4a server ftw!
+
+    <h1>stuff</h1>
+    """
+
+    # TODO: WRITE YOUR IMPLEMENTATION HERE!
+
+if __name__ == '__main__':
+    """if you'd like to test your objects in isolation (which you should),
+    you can write your tests here and run this file on its own (without
+    web_server.py)
+    """
+    pass
+    
